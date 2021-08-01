1520dfe58dc79a6ab1eec4a771486d57899395e5
Samuel Arataca <arataca89@gmail.com>
2021-08-01 13:55:25 -0300

    Add files via upload

---
+++ distancia2ptos.cpp
@@ -0,0 +1,40 @@
+// distancia2ptos.cpp
+// arataca89@gmail.com
+// Aulas de programação em C++
+
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+
+#include <cmath>
+using std::sqrt;
+using std::pow;
+
+double distancia2pontos (double x1, double y1, double x2, double y2){
+    return sqrt(pow((x1 - x2),2) + pow((y1 - y2),2));
+}
+
+
+int main(){
+    double x1,x2,y1,y2;
+    x1 = 2;
+    y1 = 3;
+    x2 = 1;
+    y2 = -2;
+
+    cout << "Distancia entre (" << x1 << "," << y1 << ") e ";
+    cout << "(" << x2 << "," << y2 << ") : "; 
+    cout << distancia2pontos(x1,y1,x2,y2) << endl;
+
+    return 0;
+}
+
+// fim de distancia2pontos.cpp
+
+/*
+SAIDA:
+
+Distancia entre (2,3) e (1,-2) : 5.09902
+
+*/
---
+++ isInside.cpp
@@ -0,0 +1,44 @@
+// isInside.cpp
+// arataca89@gmaail.com
+// Aulas de programação C++
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+// Se x estiver entre menor e maior retorna true; senão retorna false
+bool isInside(double menor, double maior, double x){
+    return (x - menor) * (x - maior) <= 0;
+}
+
+int main(){
+    double menor = -1.5;
+    double maior = 2.0;
+
+    cout << "menor: " << menor << endl;
+    cout << "maior: " << maior << endl;
+
+    cout << 0 << " esta entre " << menor << " e " << maior << "? ";
+    cout << isInside(menor, maior, 0) << endl; // 1
+
+    cout << -2.0 << " esta entre " << menor << " e " << maior << "? ";
+    cout << isInside(menor, maior, -2.0) << endl; // 0
+
+    cout << 2.5 << " esta entre " << menor << " e " << maior << "? ";
+    cout << isInside(menor, maior, 2.5) << endl; // 0
+
+    return 0;
+}
+
+// fim de isInside.cpp
+
+/*
+SAIDA:
+
+menor: -1.5
+maior: 2
+0 esta entre -1.5 e 2? 1
+-2 esta entre -1.5 e 2? 0
+2.5 esta entre -1.5 e 2? 0
+
+*/
